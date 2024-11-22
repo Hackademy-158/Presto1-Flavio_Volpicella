e49116666f99b2accda854b49363cff2f9c22aa3
Flavio Volpicella <flavio.volpicella@gmail.com>
2024-11-23 00:20:10 +0100

    Aggiunto AOS alle varie sezioni, inserito JS alla navbar, sezioni numeri incrementali ancora da terminare su JS

---
+++ resource/main.js
@@ -0,0 +1,46 @@
+let containerNavbar = document.querySelector(".container-navbar")
+let containerHeader = document.querySelector("#container-header")
+
+
+
+// scroll navbar
+window.addEventListener('scroll', ()=>{
+    let scroll = window.scrollY
+    
+    if (scroll > 0) {
+        containerNavbar.classList.remove('my-border', 'container-fluid')
+        containerNavbar.classList.add('container', 'sticky-top' , 'rounded-3')
+        containerNavbar.style.backgroundColor = 'var(--darkblue)'
+    } else {
+        containerNavbar.classList.add('my-border' , 'container-fluid')
+        containerNavbar.classList.remove('container','sticky-top' , 'rounded-3')
+        containerNavbar.style.backgroundColor = 'unset'
+    }
+})
+
+let firstNumber = document.querySelector('#firstNumber');
+let secondNumber = document.querySelector('#secondNumber');
+let thirdNumber = document.querySelector('#thirdNumber');
+let fourthNumber = document.querySelector('#fourthNumber');
+
+
+function incremento(elemento, num, tempo) {
+    let counter = 0;
+    
+    let interval = setInterval(()=>{
+        if (counter<num) {
+            counter++;
+            elemento.innerHtml = counter
+        }else{
+            clearInterval(interval);
+        }
+    },tempo)
+}
+        
+        
+
+
+incremento(firstNumber,456,100)
+incremento(secondNumber,513,100)
+incremento(thirdNumber,53,100)
+incremento(fourthNumber,17,100)
